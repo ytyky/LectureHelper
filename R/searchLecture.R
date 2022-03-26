073385cb52854f60ab82a7ed0bc37462b4e1ea2a
Tianyu Yao <yaotianyu@berkeley.edu>
2022-03-25 23:14:53 -0400

    init

---
+++ R/searchLecture.R
@@ -0,0 +1,14 @@
+#' search through lecture notes
+#'
+#' This function takes one or multiple keywords in and return related materials
+#' and locations. It searches STAT 5293 lecture slides and locate related notes
+#' and codes for query purpose.
+#' The author of STAT 5293 lecture notes is Prof.Joyce Robbins
+#'
+#' @param keywords the desire keywords
+#' @return A matrix of the related pages
+#' @export
+searchLecture <- function(keywords) {
+  results <- pdfsearch::keyword_directory('./slides', keywords)
+  results <- results[, 2:4]
+}
